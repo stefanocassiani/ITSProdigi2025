1639077111c1fde7c1e3ef50e5325afa40db0417
Daniele Simoncini <simoncini@codeninja.it>
2025-04-07 12:27:55 +0200

    Aggiungere i file di progetto.

---
+++ PrimaApp/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace PrimaApp
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            if(args.Length == 0)
+            {
+                Console.WriteLine("TABELLINE!");
+                Console.WriteLine("Per salvare sul disco la tua tabellina usa il seguente schema");
+                Console.WriteLine("applicazione.exe [tabellina] [ripetizioni]");
+                Console.WriteLine("es.: c:\\>applicazione.exe 2 10");
+            } else
+            {
+                int tabellina = int.Parse(args[0]);
+                int ripetizioni = int.Parse(args[1]);
+                for(int i=0; i < ripetizioni; i++)
+                {
+                    int risultato = tabellina * i;
+                    Console.WriteLine($"{tabellina} x {i} = {risultato}");
+                }
+            }
+            
+
+        }
+    }
+}
