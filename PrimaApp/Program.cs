c2311eb762d61324cfe1d3ba9559ecc43fc3b508
Daniele Simoncini <simoncini@codeninja.it>
2025-04-07 13:01:10 +0200

    termine prima carrellata su visual studio

--- PrimaApp/Program.cs
+++ PrimaApp/Program.cs
@@ -4,7 +4,7 @@
     {
         private static void Main(string[] args)
         {
-            if(args.Length == 0)
+            if(args.Length != 2)
             {
                 Console.WriteLine("TABELLINE!");
                 Console.WriteLine("Per salvare sul disco la tua tabellina usa il seguente schema");
@@ -12,13 +12,26 @@
                 Console.WriteLine("es.: c:\\>applicazione.exe 2 10");
             } else
             {
-                int tabellina = int.Parse(args[0]);
-                int ripetizioni = int.Parse(args[1]);
-                for(int i=0; i < ripetizioni; i++)
+                try
                 {
-                    int risultato = tabellina * i;
-                    Console.WriteLine($"{tabellina} x {i} = {risultato}");
+                    int tabellina = int.Parse(args[0]);
+                    int ripetizioni = int.Parse(args[1]);
+                    for (int i = 0; i < ripetizioni; i++)
+                    {
+                        int risultato = tabellina * i;
+                        File.AppendAllText(
+                            $"tabellina_{tabellina}.txt",
+                            $"{tabellina} X {i} = {risultato}\n"
+                        );
+                        Console.WriteLine($"{tabellina} x {i} = {risultato}");
+                    }
                 }
+                catch
+                {
+                    Console.WriteLine("Ricorda di mettere due parametri numerici!");
+                    Console.WriteLine("E ricorda di controllare quanto spazio hai sul disco!");
+                }
+                
             }
             
 
