ea089376296930f04515002ace6adf8ab6554dcd
Daniele Simoncini <simoncini@codeninja.it>
2025-04-14 17:09:52 +0200

    nuovo esercizio sulla creazione di server via httplistener

---
+++ SecondaApp/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Net;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string rootFolder = "C:\\testITS";
+
+        HttpListener server = new HttpListener();
+        server.Prefixes.Add("http://localhost:8080/");
+        server.Start();
+        Console.WriteLine("Server in ascolto...");
+        while (true)
+        {
+            // attendo una chiamata
+            HttpListenerContext chiamata = server.GetContext();
+            // leggo la richiesta
+            HttpListenerRequest cassa = chiamata.Request;
+            Console.WriteLine($"Chiamata in ingresso per {cassa.RawUrl}");
+            // ne prendo il microsofono per parlare
+            HttpListenerResponse microfono = chiamata.Response;
+            // decido cosa dire
+            // capendo se il file esiste
+            string nomeFile = rootFolder + cassa.RawUrl.Replace("/", "\\");
+            string rispostaStandard = $"Hai chiesto di vedere il file {nomeFile}";
+            if (File.Exists(nomeFile))
+            {
+                // e se esiste caricando il suo contenuto nella risposta da inviare
+                rispostaStandard = File.ReadAllText(nomeFile);
+            }            
+            // lo converto in impulsi
+            byte[] impulsi = System.Text.Encoding.UTF8.GetBytes(rispostaStandard);
+            Console.WriteLine("messaggio convertito in impulsi");
+            // e lo spedisco al mio interlocutore
+            microfono.OutputStream.Write(impulsi, 0, impulsi.Length);
+            // prima di riagganciare il telefono
+            microfono.OutputStream.Close();
+        }
+    }
+}
