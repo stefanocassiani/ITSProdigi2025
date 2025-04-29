18fcc30657a18b6a75d4d80bdd7c0c9d3256ab57
Daniele Simoncini <simoncini@codeninja.it>
2025-04-29 13:08:19 +0200

    esempio di single page application

---
+++ TerzaApp/Dati/Archivio.cs
@@ -0,0 +1,28 @@
+﻿using System.Text.Json;
+using TerzaApp.Dati.Strutture;
+
+namespace TerzaApp.Dati
+{
+    public class Archivio
+    {
+        public List<Prodotto> prodotti { get; set; }
+
+        public Archivio() {
+            if (File.Exists("prodotti.json"))
+            {
+                string json = File.ReadAllText("prodotti.json");
+                prodotti = JsonSerializer.Deserialize<List<Prodotto>>(json);
+            }
+            else
+            {
+                prodotti = new List<Prodotto>();
+            }
+        }
+
+        public void Salva()
+        {
+            string json = JsonSerializer.Serialize(prodotti);
+            File.WriteAllText("prodotti.json", json);
+        }
+    }
+}
---
+++ TerzaApp/Dati/Strutture/Prodotto.cs
@@ -0,0 +1,10 @@
+﻿namespace TerzaApp.Dati.Strutture
+{
+    public class Prodotto
+    {
+        public string Nome { get; set; } = "";
+        public string Descrizione { get; set; } = "";
+        public double Prezzo { get; set; } = 0;
+        public string Currency => $"{this.Prezzo:C2}";
+    }
+}
