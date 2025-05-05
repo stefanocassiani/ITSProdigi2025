2e6c42021eb3d8a378b0c08080f04a4cec672602
Daniele Simoncini <simoncini@codeninja.it>
2025-05-05 10:49:06 +0200

    creazione di una relazione 1-n tra categorie e prodotti

--- TerzaApp/Dati/Strutture/Prodotto.cs
+++ TerzaApp/Dati/Strutture/Prodotto.cs
@@ -5,7 +5,7 @@
         public string Nome { get; set; } = "";
         public string Descrizione { get; set; } = "";
         public double Prezzo { get; set; } = 0;
-        public Categoria? categoria { get; set; }
+        public Categoria Categoria { get; set; } = new Categoria();
         public string Currency => $"{this.Prezzo:C2}";
         
         public override string ToString()
